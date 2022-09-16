70bcec6d1fbb50f163eb396d972b330a61cdaa25
ManuWar73 <111298180+ManuWar73@users.noreply.github.com>
2022-09-16 14:11:24 +0200

    EXPERIMENTAL V2

--- src/manuwar/remodeledindustry/entities/bullet/BasicBullet.java
+++ src/manuwar/remodeledindustry/entities/bullet/BasicBullet.java
@@ -5,7 +5,7 @@ import remodeledindustry.content.*;
 import mindustry.graphics.*;
 
 public class BasicBullet extends InstantHitBulletType{
-    public CoinBulletType(float damage, float speed, float range, Color color){
+    public BasicBullet(float damage, float speed, float range, Color color){
         super(damage);
         this.pierceDamage = pierceDamage;
         hitColor = trailColor = color;
