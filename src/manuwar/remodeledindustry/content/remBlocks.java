414b1f2e0d8daf43adc289cd5ae09398fe529048
ManuWar73 <111298180+ManuWar73@users.noreply.github.com>
2022-09-16 13:10:30 +0200

    AHHHHHh

--- src/manuwar/remodeledindustry/content/remBlocks.java
+++ src/manuwar/remodeledindustry/content/remBlocks.java
@@ -71,7 +71,6 @@ public static void load() {
             coolant = consumeCoolant(0.1f);
 
             limitRange();
-        }};
              ammo(
                 Items.surgeAlloy, new BasicBulletType(4.5f, 13){{
                     ammoMultiplier = 5f;
@@ -83,22 +82,4 @@ public static void load() {
                 }}
             );
   }};
- test = new ItemTurret("test"){{
- requirements(Category.turret, with(Items.copper, 500, Items.graphite, 360, Items.metaglass, 65, Items.phaseFabric, 65));
-
- reload = 1f;
-            range = 240f;
-            recoil = 2f;
-            size = 3;
-            shoot.shots = 2;
-            shoot.shotDelay = 1f;
-            health = 140 * size * size;
-            shootY = 2f;
-            coolant = consume(consumeCoolant(1f));
-            coolantMultiplier = 0.4f;
-            shoot.type = ShootAlternate;
-            shoot.barrels = 2;
-            shoot.spread = 2;
   }};
-}
-}
