24619fd45f306fbdb3929fb5cd5cc218d18fea57
ManuWar73 <111298180+ManuWar73@users.noreply.github.com>
2022-09-16 13:07:14 +0200

    OK PLS FOR ANUKE THIS WORKS

--- src/manuwar/remodeledindustry/content/remBlocks.java
+++ src/manuwar/remodeledindustry/content/remBlocks.java
@@ -46,24 +46,32 @@ public class remBlocks{
 
 public static void load() {
   puncher = new ItemTurret("puncher"){{
- requirements(Category.turret, with(Items.copper, 500, Items.graphite, 360, Items.metaglass, 65, Items.phaseFabric, 65));
+ requirements(Category.turret, with(Items.lead, 80, Items.graphite, 65, Items.titanium, 50));
 
  reload = 1f;
             range = 240f;
             recoil = 2f;
-            size = 3;
-            shoot.shots = 2;
-            shoot.shotDelay = 1f;
+            size = 6;
             health = 140 * size * size;
             shootY = 2f;
             coolant = consume(consumeCoolant(1f));
             coolantMultiplier = 0.4f;
-            shoot = new ShootAlternate(){{
-                shots = 4;
-                barrels = 3;
-                spread = 3.5f;
-                shotDelay = 5f;
-            }};
+            shoot = new ShootAlternate(3.5f);
+
+            shootY = 3f;
+            reload = 20f;
+            range = 110;
+            shootCone = 15f;
+            ammoUseEffect = Fx.casing1;
+            health = 250;
+            shots = 2f;
+            shotDelay = 1f;
+            inaccuracy = 0f;
+            rotateSpeed = 10f;
+            coolant = consumeCoolant(0.1f);
+
+            limitRange();
+        }};
              ammo(
                 Items.surgeAlloy, new BasicBulletType(4.5f, 13){{
                     ammoMultiplier = 5f;
