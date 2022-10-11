1d9edc461348c10c200ea34728d5927d8d259cbb
ManuWar73 <manuelsf2009@gmail.ccom>
2022-10-11 16:43:52 +0200

    Voider try 1

--- src/remodeledindustry/entities/bullet/BasicBullet.java
+++ src/remodeledindustry/entities/bullet/BasicBullet.java
@@ -1,7 +1,16 @@
 package remodeledindustry.entities.bullet;
 
-import mindustry.entities.bullet.BasicBulletType;
+import mindustry.entities.bullet.BulletType;
 
-public class BasicBullet extends BasicBulletType{
-
+public class BasicBullet extends BulletType {
+    public BasicBullet(float speed, float damage) {
+        super(speed, damage);
+        hittable = absorbable = false;
+        collides = collidesAir = collidesGround = collidesTiles = false;
+        pierce = true;
+    }
+    @Override
+    public float continuousDamage(){
+        return damage / 2f * 60f;
+    }
 }
