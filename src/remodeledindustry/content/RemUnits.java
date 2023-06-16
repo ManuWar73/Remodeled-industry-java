6c9ea27b4ca106773401c6a049a16fa9d0e1b320
ManuWar73 <manuelsf2009@gmail.ccom>
2023-06-16 21:54:38 +0200

    Lightstrike

--- src/remodeledindustry/content/RemUnits.java
+++ src/remodeledindustry/content/RemUnits.java
@@ -1,20 +1,26 @@
 package remodeledindustry.content;
 
 import arc.graphics.Color;
+import mindustry.ai.UnitCommand;
 import mindustry.content.Fx;
 import mindustry.content.Items;
 import mindustry.content.StatusEffects;
+import mindustry.entities.abilities.MoveLightningAbility;
+import mindustry.entities.abilities.RepairFieldAbility;
 import mindustry.entities.bullet.BulletType;
 import mindustry.entities.bullet.FlakBulletType;
 import mindustry.entities.bullet.LaserBulletType;
+import mindustry.entities.bullet.MissileBulletType;
 import mindustry.gen.Sounds;
+import mindustry.graphics.Pal;
 import mindustry.type.UnitType;
 import mindustry.type.Weapon;
 import mindustry.type.ammo.ItemAmmoType;
+import mindustry.type.ammo.PowerAmmoType;
 import mindustry.world.meta.BlockFlag;
 
 public class RemUnits {
-    public static UnitType teeclipse;
+    public static UnitType teeclipse, lspoly;
     public void load(){
         teeclipse = new UnitType("thousand-embers-eclipse"){{
             localizedName = "Thousand Embers Eclipse";
@@ -95,6 +101,62 @@ public class RemUnits {
                         shootY = 7.25f;
                         bullet = fragBullet;
                     }});
+        }};
+        lspoly = new UnitType("lightstrike-poly"){{
+            defaultCommand = UnitCommand.rebuildCommand;
+
+            flying = true;
+            localizedName = "LightStrike Poly";
+            drag = 0.05f;
+            speed = 2.6f;
+            rotateSpeed = 15f;
+            accel = 0.1f;
+            range = 130f;
+            health = 400;
+            buildSpeed = 0.5f;
+            engineOffset = 6.5f;
+            hitSize = 9f;
+            lowAltitude = true;
+
+            ammoType = new PowerAmmoType(900);
+
+            mineTier = 2;
+            mineSpeed = 3.5f;
+
+            abilities.add(new MoveLightningAbility(70, 8, 0.5f, 0, 1, 10, Color.yellow));
+
+            weapons.add(new Weapon("remodeled-ind-ls-poly-weapon"){{
+                top = false;
+                y = -2.5f;
+                x = 3.75f;
+                reload = 20f;
+                ejectEffect = Fx.none;
+                recoil = 2f;
+                shootSound = Sounds.missile;
+                velocityRnd = 0.5f;
+                inaccuracy = 15f;
+                alternate = true;
+
+                bullet = new MissileBulletType(4f, 28){{
+                    homingPower = 0.08f;
+                    weaveMag = 5;
+                    weaveScale = 5;
+                    splashDamage = 10;
+                    splashDamageRadius = 16;
+                    lifetime = 80f;
+                    keepVelocity = false;
+                    accel = 0.02f;
+                    shootEffect = Fx.shootPyraFlame;
+                    smokeEffect = Fx.hitLaser;
+                    hitEffect = despawnEffect = Fx.hitLaser;
+                    frontColor = Color.red;
+                    hitSound = Sounds.none;
+
+                    backColor = Pal.turretHeat;
+                    trailColor = Pal.turretHeat;
+                    hitEffect = despawnEffect = Fx.explosion;
+                }};
+            }});
         }};
     }
 
